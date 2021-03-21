3f34594ef60a44c868f011d697f1746daf6edd6e
Blace <blace.rum93@gmail.com>
2021-03-21 23:06:31 +0300

    Corrected unittests

--- unittest_to_bp.py
+++ unittest_to_bp.py
@@ -4,15 +4,20 @@ from blueprint import make_request
 
 class BlueprintFunctionTest(unittest.TestCase):
     def test_make_request(self):
-        self.assertEqual(make_request('12ksasnnas'), 'Something go wrong')
+        self.assertEqual(make_request('12ksasnnas'),
+                         'Something goes wrong, try another one')
         self.assertEqual(make_request('Арзамас'),
                          'Entered address is 474.19km away from MKAD')
         self.assertEqual(make_request('Москва'),
                          'Entered address is in MKAD-area')
-        self.assertEqual(make_request('Kansas'), 'Something go wrong')
-        self.assertEqual(make_request('       '), 'Something go wrong')
-        self.assertEqual(make_request(123), 'Something go wrong')
-        self.assertEqual(make_request([123]), 'Something go wrong')
+        self.assertEqual(make_request('Kansas'),
+                         'Something goes wrong, try another one')
+        self.assertEqual(make_request('       '),
+                         'Something goes wrong, try another one')
+        self.assertEqual(make_request(123),
+                         'Something goes wrong, try another one')
+        self.assertEqual(make_request([123]),
+                         'Something goes wrong, try another one')
 
 
 if __name__ == "__main__":
